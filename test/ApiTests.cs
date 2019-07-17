44137e4a44a028d70a9fe1102b0659dda1d8032d
Peter Lobley <peter.lobley@gmail.com>
2019-07-17 20:25:01 +1000

    api test

---
+++ test/ApiTests.cs
@@ -0,0 +1,30 @@
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net.Http;
+using System.Net;
+using System;
+using Xunit;
+using www;
+
+namespace test
+{
+    public class ApiTests
+    {
+        [Fact]
+        public async Task CanCallApi()
+        {
+            CancellationTokenSource cancel = new CancellationTokenSource();
+            var server = new ApiServer(8080).RunAsync(cancel.Token);
+            await Task.Delay(1000);
+
+            var request = HttpWebRequest.CreateHttp("http://localhost:8080/dostuff");
+            request.Method = HttpMethod.Get.Method;
+            var response = (HttpWebResponse) request.GetResponse();
+            
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            cancel.Cancel();
+            await server;
+        }
+    }
+}
---
+++ www/ApiServer.cs
@@ -0,0 +1,16 @@
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace www
+{
+    public class ApiServer
+    {
+        public ApiServer(int port) {
+
+        }
+
+        public async Task RunAsync(CancellationToken token) {
+
+        }
+    }
+}
