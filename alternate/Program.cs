9394657e5bbacd0872b8bb683e614ed750b99ee6
Peter Lobley <peter.lobley@gmail.com>
2019-07-17 19:56:16 +1000

    skeleton

---
+++ alternate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace alternate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ www/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace www
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseKestrel(o =>
+                {
+                    o.Listen(new IPEndPoint(IPAddress.Any, 8080));
+                })
+                .ConfigureServices(serviceCollection => {
+                    serviceCollection.AddMvc();
+                    serviceCollection.AddRouting();
+                })
+                .Configure(appBuilder => {
+                });
+    }
+}
